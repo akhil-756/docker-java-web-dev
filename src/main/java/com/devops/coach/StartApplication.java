50cc39056d3ccc906a961395fb7e4dea7bb4d5e4
akhil-756 <137878790+akhil-756@users.noreply.github.com>
2023-10-11 01:42:38 +0530

    Update StartApplication.java

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce;
+package com.devops.coach;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,30 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
-public class EcommerceApplication {
+public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Welcome to Our E-commerce Store");
-        model.addAttribute("msg", "Browse our products and start shopping!");
+        model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!!");
         return "index";
     }
 
-    @GetMapping("/products")
-    public String productList(final Model model) {
-        // Retrieve a list of products from your database or service
-        // Add the list of products to the model
-        return "product-list";
-    }
-
-    @GetMapping("/product/{id}")
-    public String productDetail(final Model model, @PathVariable Long id) {
-        // Retrieve product details based on the given ID
-        // Add product details to the model
-        return "product-detail";
-    }
-
     public static void main(String[] args) {
-        SpringApplication.run(EcommerceApplication.class, args);
+        SpringApplication.run(StartApplication.class, args);
     }
+
 }
